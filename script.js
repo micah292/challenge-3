6c1a3e2b1be0a5e82a200edcc89883253fff5f20
micah292 <sharifimicah17@gmail.com>
2022-12-01 19:27:16 -0700

    continue file

--- script.js
+++ script.js
@@ -7,17 +7,18 @@ var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", 
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  // prompt per var selected
-  // var lengthPassword=prompt("Choose a length of at least 8 characters and no more than 128 characters.")
 
+  //parseInt = input characters
   var userInput = parseInt(
     prompt("how many characters would you like?"), 10
   )
+  // is nan= not a number
   if (Number.isNaN(userInput)) {
     alert("you must enter a number")
     return null
   }
-
+  // null = nothing (not necessary but good practice)
+  // user input is what user value is entered
   if (userInput > 128) {
     alert("you must enter less than 128")
     return null
@@ -26,23 +27,23 @@ function generatePassword() {
     alert("you must enter greater than 8")
     return null
   }
-
+  // how we show confirm or cancel 
   var confirmUppercase = confirm("Do you want upperCase?")
   var confirmNumbers = confirm("Do you want numbers?")
   var confirmLowerCase = confirm("Do you want lower case?")
   var confirmSymbols = confirm("Do you want symbols?")
 
-  // ! makes falesy
-  // && ampersand
+  // ! in front of makes falesy
+  // && ampersand always use when all is false
   if (
     !confirmUppercase && !confirmNumbers && !confirmLowerCase && !confirmSymbols
   ) {
-    alert(" Must pick one")
+    alert("Must pick one")
     return null
   }
   // null = nothing
-  // store user input
-  // var *key name* = {1 object var: same object var} 
+  // storage user input 
+  // var *keyStorageName* = {1 object var: same object value} 
   var inputStore = {
     userInput: userInput,
     confirmUppercase: confirmUppercase,
@@ -51,8 +52,71 @@ function generatePassword() {
     confirmSymbols: confirmSymbols
 
   }
+  //will resturn inputStore var
   return inputStore
 }
+//randomizer selcector
+function randomizer(charz) {
+  var randomChar = Math.floor(Math.random() * charz.length)
+  var randomEl = charz[randomChar]
+  return randomEl
+  //will return random var
+}
+
+var checkArrays
+
+//function create password inside store generate password inside var, create 3 empty arrays 
+//(1 will store result) 
+//2 will store posidble chars)
+//3 will have definite chars)
+// conditonal logic (if else) (switch case)
+// run check arrays = user input u,l,s,
+// push to empty array *.push method*
+//
+
+function createPassword() {
+  var result = [];
+  var possibleChars = [];
+  var definiteChars = [];
+
+
+
+  var choices = generatePassword()
+  if (!choices) {
+    return null
+  }
+  if (!choices.confirmUppercase) {
+    possibleChars = possibleChars + upperCase
+    definiteChars.push(randomizer(upperCase))
+  }
+
+  if (!choices.confirmLowerCase) {
+    possiblesChars = possibleChars + lowerCase
+    definiteChars.push(randomizer(lowerCase))
+  }
+
+  if (!choices.confirmNumbers) {
+    possibleChars = possibleChars + numbers
+    definiteChars.push(randomizer(numbers))
+  }
+    if (!choices.confirmSymbols) {
+      possibleChars = possibleChars + symbols
+      definiteChars.push(randomizer(symbols))
+    }
+
+  }
+
+
+
+
+}
+
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
